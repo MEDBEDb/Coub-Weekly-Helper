04a1cf09fcc8bb653f5364b5f65e714373155755
Sveatoslav Circel <MEDBEDb@me.com>
2016-03-04 12:29:30 +0200

    removed redundant code

--- get_weekly.rb
+++ get_weekly.rb
@@ -1,11 +1,9 @@
 require "date"
 
-@exec = "";
-
 if Gem::Platform.local.to_s.include? "mingw32"
-	@exec = "start"
+  @exec = "start"
 else 
-	@exec = "open"
+  @exec = "open"
 end
 
 %x[#{@exec} "http://coub.com/weekly/#{Date.today.strftime("%Y")}/#{Date.today.strftime("%W").to_i-1}"]
